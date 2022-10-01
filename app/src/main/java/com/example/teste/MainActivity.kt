afbdd74471f9e1caeb9f93b5b5fb12a145901991
JoshuaJones27 <100417295+JoshuaJones27@users.noreply.github.com>
2022-10-01 17:26:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/teste/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
