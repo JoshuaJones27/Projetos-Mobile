8d8cd0f26f1e8a7e258e98651bff074634c35623
Joshua Jones <a21116@alunos.ipca.pt>
2022-10-11 15:34:57 +0100

    Falta o bem vindo e outras toques

---
+++ app/src/main/java/com/example/teste/EcraRegisto.kt
@@ -0,0 +1,11 @@
+package com.example.teste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EcraRegisto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ecra_registo)
+    }
+}
--- app/src/main/java/com/example/teste/MainActivity.kt
+++ app/src/main/java/com/example/teste/MainActivity.kt
@@ -1,12 +1,40 @@
-package com.example.firstapp
+package com.example.teste
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var login: Button
+    private lateinit var Email : EditText
+    private lateinit var register : Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
+        setContentView(R.layout.activity_main)
 
+        login = findViewById(R.id.button_login)
+        Email = findViewById(R.id.main_email_ph_et)
+        register = findViewById(R.id.button_registar)
+
+        login.setOnClickListener{
+            val email = Email.text.toString().trim()
+
+            if (email.isEmpty()){
+                Email.error = "Please introduce an email"
+            } else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                Email.error = "Email not valid"
+            }
+        }
+
+        register.setOnClickListener{
+            val intent = Intent(this@MainActivity, Register::class.java)
+            startActivity(intent)
+        }
+    }
 }
