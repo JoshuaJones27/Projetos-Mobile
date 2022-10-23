858c665e27c50cbe7a171b68535d700dcac9c5cd
Joshua Jones <a21116@alunos.ipca.pt>
2022-10-23 15:10:40 +0100

    Receba meu deus primeiro trabalho completo

--- app/src/main/java/com/example/teste/EcraRegisto.kt
+++ app/src/main/java/com/example/teste/EcraRegisto.kt
@@ -1,11 +1,53 @@
 package com.example.teste
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class EcraRegisto : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecra_registo)
     }
+
+    fun registerClick(v: View) {
+
+        val emailEditText : EditText = findViewById(R.id.register_email_ph_et)
+        val passwordEditText : EditText = findViewById(R.id.register_password_ph_et)
+        val confPasswordEditText : EditText = findViewById(R.id.register_confirm_password_ph_et)
+
+        val email : String = emailEditText.text.toString()
+        val password : String = passwordEditText.text.toString()
+        val confPassword : String = confPasswordEditText.text.toString()
+
+        val emailRegExp = Regex("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}\$")
+        val passwordRegExp = Regex("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}\$")
+
+        if(email.isBlank() || !emailRegExp.matches(email)) {
+            val error = Toast.makeText(this, "Invalid email.", Toast.LENGTH_LONG)
+            error.show()
+            return
+        }
+
+        if (password.isBlank() || !passwordRegExp.matches(password)) {
+            val error = Toast.makeText(this, "Invalid password.", Toast.LENGTH_LONG)
+            error.show()
+            return
+        }
+
+        if(password != confPassword) {
+            val error = Toast.makeText(this, "Passwords are different.", Toast.LENGTH_LONG)
+            error.show()
+            return
+        }
+
+        val mainActivity : Intent = Intent(this, MainActivity::class.java)
+        startActivity(mainActivity)
+    }
+
+
 }
--- app/src/main/java/com/example/teste/MainActivity.kt
+++ app/src/main/java/com/example/teste/MainActivity.kt
@@ -3,39 +3,24 @@ package com.example.teste
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var login: Button
-    private lateinit var Email : EditText
-    private lateinit var register : Button
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        login = findViewById(R.id.button_login)
-        Email = findViewById(R.id.main_email_ph_et)
-        register = findViewById(R.id.button_registar)
-
-        login.setOnClickListener{
-            val email = Email.text.toString().trim()
-
-            if (email.isEmpty()){
-                Email.error = "Please introduce an email"
-            } else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                Email.error = "Email not valid"
-            }
-        }
-
-        register.setOnClickListener{
-            val intent = Intent(this@MainActivity, register::class.java)
-            startActivity(intent)
-        }
     }
 
+    fun loginClick(v: View) {
+        val bemvindoActivity : Intent = Intent(this, BemVindo::class.java)
+        startActivity(bemvindoActivity)
+    }
+
+    fun registerClick(v: View) {
+        val registerActivity : Intent = Intent(this, EcraRegisto::class.java)
+        startActivity(registerActivity)
+    }
 }
